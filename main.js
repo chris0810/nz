f5d82787642696511d19bb36e83bc279f70d1ea2
chris0810 <you@example.com>
2023-03-30 16:17:51 +0200

    array

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-<script>
-    var map = L.map('map').setView([-43.883333, 170.516667], 13);
+let stop_lat = -43.883333;
+let stop_len = 170.516667;
+let zoom= 13;
+let title= 'Lake Tekapo';
+let map = L.map('map').setView([stop_lat,stop_len], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+}).addTo(map);
 
-L.marker([-43.883333, 170.516667]).addTo(map)
-    .bindPopup('Lake Tekapo')
+L.marker([stop_lat,stop_len]).addTo(map)
+    .bindPopup(title)
     .openPopup();    
-    </script> 
